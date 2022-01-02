01d0bfa1db7c330014d9d35f194ed75f19a3c3b9
Marcus Willock <crazcalm@gmail.com>
2022-01-02 18:52:36 -0500

    02_01e video code

--- 02_01/think_about_option/src/main.rs
+++ 02_01/think_about_option/src/main.rs
@@ -2,11 +2,11 @@
 struct Invitation {
     invitee: String,
     attending: bool,
-    message: String,
+    message: Option<String>,
 }
 
 impl Invitation {
-    fn new(invitee: String, attending: bool, message: String) -> Invitation {
+    fn new(invitee: String, attending: bool, message: Option<String>) -> Invitation {
         Invitation {
             invitee,
             attending,
@@ -16,5 +16,9 @@ impl Invitation {
 }
 
 fn main() {
+    let invitation_1 = Invitation::new("Dolores".to_string(), true, None);
+    let invitation_2 = Invitation::new("Cosmo".to_string(), false, Some("Sorry. I am flying to Mars that week".to_string()));
 
+    println!("{:#?}", invitation_1);
+    println!("{:#?}", invitation_2);
 }
