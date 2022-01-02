24ce496856473c3e73ee5aca9fa34b688d7b656a
Marcus Willock <crazcalm@gmail.com>
2022-01-02 16:54:34 -0500

    01_01 part 1

---
+++ 01_01/todo_karaoke/src/main.rs
@@ -0,0 +1,27 @@
+use std::io::Error;
+
+trait Karaoke {
+    fn pick_song_i_know(&self, songs_i_know: Vec<String>, song_list: Vec<String>) -> Option<String>;
+
+    fn sing(&self, song: String) -> Result<(), Error>;
+}
+
+struct Bubbles {}
+
+impl Karaoke for Bubbles {
+    fn pick_song_i_know(&self, songs_i_know: Vec<String>, song_list: Vec<String>) -> Option<String>{
+        let mut picked_song = None;
+
+        for song in song_list {
+            if songs_i_know.contains(&song){
+                picked_song = Some(song);
+                break;
+            }
+        }
+        picked_song
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
