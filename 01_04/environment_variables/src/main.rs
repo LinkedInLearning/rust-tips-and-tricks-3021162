b089795d1de5097fcda03aa1392183d4c1b5061a
Marcus Willock <crazcalm@gmail.com>
2022-01-25 08:46:16 -0500

    01_04: update user to username

--- 01_04/environment_variables/src/main.rs
+++ 01_04/environment_variables/src/main.rs
@@ -1,17 +1,17 @@
-fn third_party_api(user: &str, password: &str) -> String {
+fn third_party_api(username: &str, password: &str) -> String {
     /* Makes an outbound request to connet to the
      * server
      */
 
-    let mut access_token = user.to_string();
+    let mut access_token = username.to_string();
     access_token.push_str(password.split_at(2).0);
 
     access_token
 }
 
 fn main() {
-    let user = "Elton".to_string();
+    let username = "Elton".to_string();
     let password = "my_password".to_string();
 
-    println!("Access Token: {}", third_party_api(&user, &password))
+    println!("Access Token: {}", third_party_api(&username, &password))
 }
